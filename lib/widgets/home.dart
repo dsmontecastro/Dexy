4bbda592241863e05ca8c5c061570af43665334a
D2 <dsmontecastro1@up.edu.ph>
2023-09-05 23:17:10 +0800

    [HOME] Globalized ScrollBehavior

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:pokedex/extensions/providers.dart';
 
@@ -9,7 +10,12 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(context) {
+    final scrollBehavior = ScrollConfiguration.of(context).copyWith(
+      dragDevices: {PointerDeviceKind.touch, PointerDeviceKind.mouse},
+    );
+
     return MaterialApp(
+      scrollBehavior: scrollBehavior,
       home: Scaffold(
         key: context.key,
         body: const Body(),
