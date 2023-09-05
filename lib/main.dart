590f43eea2cf146d0fae8539a3fbb2f5f9ce7b1e
D2 <dsmontecastro1@up.edu.ph>
2023-09-05 22:54:12 +0800

    [-] Added Windows Manager & FL Charts

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,43 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:window_manager/window_manager.dart';
 
 import 'prefs/prefs.dart';
 import 'widgets/home.dart';
 import 'extensions/providers.dart';
 
+const WindowOptions windowOptions = WindowOptions(
+  backgroundColor: Colors.transparent,
+  minimumSize: Size(400, 600),
+  size: Size(800, 600),
+  skipTaskbar: false,
+  center: true,
+  title: "Dex",
+);
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SharedPrefs.init();
 
-  // Extra configuration for PC SQFlite
   if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    // Extra configuration for Desktops
+
+    // PC SQFlite
     sqfliteFfiInit();
     databaseFactory = databaseFactoryFfi;
+
+    // Windows Manager
+    await windowManager.ensureInitialized();
+    windowManager.waitUntilReadyToShow(
+      windowOptions,
+      () async {
+        await windowManager.show();
+        await windowManager.focus();
+      },
+    );
+
+    //
   }
 
   runApp(const Wrapper());
